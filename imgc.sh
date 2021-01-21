5a677f0dcfb2624597c07acf643e33b06a3c5ddf
Valentin Soare <soarevalentinn@gmail.com>
2021-01-21 03:45:17 +0200

    Code rewritten. Still under development..

--- imgc.sh
+++ imgc.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/bash
 
 declare -a filename=("$@")
-declare -x desired_extension
-
-modifyArgPosition() {
-    if printf "%s" "${1}" | grep -Eq "\-o"; then
-        shift 1    
-        desired_extension=".${1}"
-    else
-        desired_extension=".jpg"
-    fi
-}
 
 checkingSw() {
     if ! cmdChk="$(yum list installed ImageMagick 2> /dev/null)"; then
@@ -22,22 +12,51 @@ checkingSw() {
     fi
 }
 
-main() {
-    modifyArgPosition "$@"
-    checkingSw
-    printf "\n"
+converting() {
+   local desired_extension="$1"
 
-    for ((i=2;i<${#filename[@]};i++)); do
+    if [[ ${#desired_extension} -ne 3 ]]; then
+        printf "\n\e[1;31m%s\e[0m\n\n" "ERROR - You need to mentioned a desired extension for output."
+        exit 1
+    fi
+
+   printf "%60s\n" " " | tr ' ' '-'
+   for ((i=2;i<${#filename[@]};i++)); do
         extension="${filename[i]#*.}"
         output_name="${filename[i]%.*}"
 
         if [[ "${extension}" == "${desired_extension}" ]]; then
-            printf "\n\e[1;31m%s\e[0m\n\n" "*Image is already in JPG format."
+            printf "\n\e[1;31m%s\e[0m\n\n" "*Image is already in ${desired_extension} format."
         else
-            convert "${filename[i]}" "${output_name}${desired_extension}"
-            printf "\e[1;32m%s\e[0m\n\n" "*${filename[i]} converted to ${output_name}${desired_extension}"
+            convert "${filename[i]}" "${output_name}.${desired_extension}"
+            printf "\e[1;32m%s\e[0m\n" "*${filename[i]} converted to ${output_name}.${desired_extension}"
         fi
+    done
+    printf "%60s\n" " " | tr ' ' '-'
+}
+
+identifiying() {
+    printf "%60s\n" " " | tr ' ' '-'
+    for ((i=1;i<${#filename[@]};i++)) {
+        printf "\e[32m%s\e[0m\n" "$(identify "${filename[i]}")"
+    }
+    printf "%60s\n" " " | tr ' ' '-'
+}
+
+main() {
+    local usage="Usage: $0 [-i] [-c extension] imagefiles"
+    checkingSw
+    while getopts ":ic:" option_given; do
+        case "${option_given}" in
+            i)  identifiying
+                ;;
+            c)  converting "${OPTARG}"
+                ;;     
+            \?) printf "\n\e[1;31m%s\e[0m\n\n" "${usage}"
+                exit 1
+                ;;
+        esac
     done
 }
 
-main "${filename[@]}"
+main "$@"
